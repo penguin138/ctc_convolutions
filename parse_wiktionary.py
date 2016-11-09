91a5eaa80e6f4e332606b897ef93bb21f5dadf0b
penguin138 <fridaythethirteens@gmail.com>
2016-11-09 19:21:44 +0300

    add first version of syllable parser

---
+++ parse_wiktionary.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+from bs4 import BeautifulSoup
+import re
+import sys
+import time
+
+
+def parse_matches(matches):
+    parsed = []
+    for match in matches:
+        start_idx = match.find('слоги={{по-слогам|')
+        match = match[start_idx + 18:-2]
+        parsed.append(" ".join(match.split('|')))
+    return parsed
+
+
+def parse(filename):
+    with open(filename) as wiktionary:
+        print("Reading xml...")
+        start = time.time()
+        soup = BeautifulSoup(wiktionary, "xml")
+        end = time.time()
+        print('Done in {} seconds'.format(end - start))
+        print("Searching for pages...")
+        pages = soup.findAll("page")
+        print("Parsing pages...")
+        with open("parsed_syllables.txt", 'w') as out_file:
+            for page in pages:
+                text = page.find("text")
+                title = page.find("title")
+                title_text = str(title.string)
+                tag_text = str(text.string)
+                vowels = re.findall(r'[аеиоуыэёюя]', title_text)
+                print("Processing tag {}".format(str(title.string)))
+                if re.search(r'язык=ru', tag_text):
+                    if len(vowels) > 1:
+                        matches = re.findall(r'слоги=\{\{по-слогам\|.*?}\}',
+                                             tag_text)
+                        parsed = parse_matches(matches)
+                    else:
+                        parsed = [title_text]
+                    if len(parsed) >= 1:
+                        out_file.write(title_text + "\t" + " | ".join(parsed) +
+                                       '\n')
+
+if __name__ == "__main__":
+    parse(sys.argv[1])
